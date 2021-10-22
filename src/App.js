4974d2e2c9917e50f009aeb183a75d63f5861efa
Abhishek Kumar <abhishekk580@gmail.com>
2021-10-22 22:21:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Responsive, WidthProvider } from "react-grid-layout";
+import { useState } from "react";
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 function App() {
+  const [layout, setLayout] = useState([
+    { i: "a", x: 0, y: 0, w: 1, h: 2 },
+    { i: "b", x: 1, y: 0, w: 3, h: 2 },
+    { i: "c", x: 4, y: 0, w: 1, h: 2 },
+  ]);
+
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const addGrid = () => {
+    const grid = {
+      i: Date(),
+      x: getRandomInt(1, 10),
+      w: getRandomInt(1, 5),
+      h: 2,
+    };
+    setLayout((prevState) => [...prevState, grid]);
+  };
+
+  const onDelete = (id) => {
+    const arr = layout.filter((data) => (data.i === id ? false : true));
+    setLayout(arr);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={addGrid}>Add new grid</button>
+        <hr />
+      </div>
+      <ResponsiveReactGridLayout className="layout">
+        {layout.map((data) => {
+          return (
+            <div key={data.i} style={{ border: "1px solid" }}>
+              <p>{data.i}</p>
+              <button onClick={() => onDelete(data.i)}>delete</button>
+            </div>
+          );
+        })}
+      </ResponsiveReactGridLayout>
     </div>
   );
 }
